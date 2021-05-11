d34edacd68197f9d4e1ccd20a3ed39643d9f3525
sfaridi1 <70887855+sfaridi1@users.noreply.github.com>
2021-05-11 11:12:25 +0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private iab: InAppBrowser,) {}
+
+
+  openBlank(){
+this.iab.create('http://www.sdrs.com.sa','_blank','location=yes');
+  }
+  openSystem(){
+    this.iab.create('http://www.sdrs.com.sa','_system','location=yes');
+  }
 
 }
